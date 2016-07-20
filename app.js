fdd84abadb5cd1a497dd7f3d287942cf44abf050
Laurence Morin-Daoust <laurence.morin.daoust@gmail.com>
2016-07-19 22:02:47 -0400

    Config added.

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var app     = express();
+var config  = require('config');
+
 
 console.log(__dirname);
 app.use(express.static(__dirname + '/'));
+
+if(process.env.FLICKR_KEY && process.env.FLICKR_PASS) {
+  console.log("flickr_key  ==> " + process.env.FLICKR_KEY);
+  console.log("flickr_pass ==> " + process.env.FLICKR_PASS);
+} else {
+  console.log("flickr_key  ==> " + config.get("flickr.FLICKR_KEY"));
+  console.log("flickr_pass ==> " + config.get("flickr.FLICKR_PASS"));
+}
+
 
 
 app.get('/casse-tete/*', function(req, res) {
