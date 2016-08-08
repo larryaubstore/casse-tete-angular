dfa68f34b0fdd9e7183df916c3a0245304949d54
Laurence Morin-Daoust <laurence.morin.daoust@gmail.com>
2016-08-07 21:48:56 -0400

    Missing file added.

---
+++ app/tilesetup.js
@@ -0,0 +1,10 @@
+"use strict";
+var TileSetup = (function () {
+    function TileSetup(incX, incY, puzzles) {
+        this.incX = incX;
+        this.incY = incY;
+        this.puzzles = puzzles;
+    }
+    return TileSetup;
+}());
+exports.TileSetup = TileSetup;
